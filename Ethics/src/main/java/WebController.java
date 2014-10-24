bfd22a92836693a8a951305d598108b7f42e083e
danlaptop <barrell12@gmail.com>
2014-10-24 14:20:57 +0100

    FrontEnd Changes matt is a faggot for reals

--- Ethics/src/main/java/WebController.java
+++ Ethics/src/main/java/WebController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/guardian")
 public class WebController {
     private static final Logger logger = Logger.getLogger(WebController.class);
     private static final Manager manager = new Manager();
 
-    @RequestMapping(value="/*", method = RequestMethod.GET)
+    @RequestMapping(value="/ei", method = RequestMethod.GET)
     public static String getGuardianPoliticalArticle(ModelMap model){
         ConvertedPojo pojo = manager.getConvertedPojo();
         model.addAttribute("attribute", pojo.getAttribute());
